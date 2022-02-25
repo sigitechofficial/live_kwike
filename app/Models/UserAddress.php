ab9f4c9ab2c5c5d317007b58ada3ec32ae8c49bf
nadeem <96167120+sigitechofficial@users.noreply.github.com>
2022-02-25 19:06:03 +0500

    UserAddress model error rectified

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserAdrress extends Model
+class UserAddress extends Model
 {
     use HasFactory;
     protected $fillable=['user_id','latitude','longitude','address','title','city','flat_no','postal_code'];
